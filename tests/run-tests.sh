f5bb8fe95d89e7923e1de64443b905568de6e2ef
Michael Rees <reesmichael1@vivaldi.net>
2016-05-30 10:54:11 -0700

    Add tests for util.rkt

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/sh
 
+echo "diff-tests"
 racket diff-tests.rkt
+echo "newton-sqrt-tests"
 racket newton-sqrt-tests.rkt
+echo "newton-root-tests"
 racket newton-root-tests.rkt
+echo "euler-method-tests"
 racket ode-tests/euler-method-tests.rkt
+echo "util-tests"
+racket util-tests.rkt
---
+++ tests/util-tests.rkt
@@ -0,0 +1,19 @@
+#lang racket/base
+
+; Test utility functions for NumeriKet
+
+(require rackunit rackunit/text-ui "../NumeriKet/util.rkt")
+
+(define util-tests
+    (test-suite
+        "Test round-to-precision"
+        (check-equal? (round-to-precision 10 3) 10 "Rounds integers")
+        (check-equal? (round-to-precision 0.17832 2) 0.18 "Rounds decimals")
+        (check-equal? (round-to-precision 13.878 0) 14.0
+                      "Rounds to 0 decimals")
+        (check-equal? (round-to-precision -inf.f 10) -inf.f 
+                      "Rounds infinite cases")
+        (check-equal? (round-to-precision -234.871034 50) -234.871034 
+                      "Rounds more decimals than are in input")))
+
+(run-tests util-tests)
