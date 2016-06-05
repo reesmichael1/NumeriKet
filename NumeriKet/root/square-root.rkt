c6788c36776c3a06d50b90d80bdd1e635a20d701
reesmichael1 <reesmichael1@vivaldi.net>
2016-06-04 17:02:54 -0700

    Refactor closest integer root to O(log n)

--- NumeriKet/root/square-root.rkt
+++ NumeriKet/root/square-root.rkt
@@ -1,23 +1,29 @@
 #lang racket/base
 
 ; Provide implementation of square root calculationg
-(provide square-root)
+(provide square-root closest-integer-sqrt)
 
 (require NumeriKet/root/newton-sqrt)
 
 ; function: closest-integer-sqrt
 ; Inputs:
 ;       * x: the number (>= 0) whose closest perfect square is sought
-;       * n: the integer that is checked to be a perfect square of x
+;       * nMin: the integer forming a lower bound on the interval to check
+;       * nMax: the integer forming an upper bound on the interval to check
 ; Output: an integer y such that y^2 <= x, but (y + 1)^2 > x
-(define (closest-integer-sqrt x n)
-    (if (< x (expt (+ n 1) 2)) n (closest-integer-sqrt x (+ n 1))))
+(define (closest-integer-sqrt x nMin nMax)
+  (if (< (- nMax nMin) 2)
+    (if (> (expt nMax 2) x) nMin nMax)
+    (let* [(nMid (floor (/ (+ nMin nMax) 2)))]
+       (if (> (expt nMid 2) x) 
+         (closest-integer-sqrt x nMin nMid) 
+         (closest-integer-sqrt x nMid nMax) ))))
 
 ; function: square-root
 ; Inputs:
 ;       * x: the number whose square root will be calculated
 ; Output: the square root of x
 (define (square-root x)
-    (if (> 0 x) (error "sqrt: input must be non-negative")
-        (if (= 0 x) 0
-          (newton-sqrt x (closest-integer-sqrt x 0)))))
+  (if (> 0 x) (error "sqrt: input must be non-negative")
+    (if (= 0 x) 0
+      (newton-sqrt x (closest-integer-sqrt x 0 x)))))
--- tests/root-tests/square-root-tests.rkt
+++ tests/root-tests/square-root-tests.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(require rackunit rackunit/text-ui "../test.rkt" 
+(require rackunit rackunit/text-ui racket/math "../test.rkt" 
          "../../NumeriKet/root/square-root.rkt")
 
 (define square-root-tests
@@ -15,7 +15,23 @@
                     "Calculates large non-perfect squares well")
         (check-equal? (square-root 0) 0 "Calculates square root of 0")
         (check-exn exn:fail? (lambda () (square-root -1)) 
-                   "Rejects negative input")
-        ))
+                   "Rejects negative input")))
 
-(run-tests square-root-tests)
+(define closest-integer-sqrt-tests
+    (test-suite
+        "closest-integer-sqrt-tests"
+        (check-equal? (closest-integer-sqrt 4 0 4) 2 
+                      "Correct for perfect squares")
+        (check-equal? (closest-integer-sqrt 23 0 23) 4
+                      "Correct for imperfect square integers")
+        (check-equal? (closest-integer-sqrt 97.4 0 97.4) 9.0
+                      "Correct for imperfect square rational numbers")
+        (check-equal? (closest-integer-sqrt pi 0 pi) 1.0
+                      "Correct for irrational numbers")
+        (check-equal? (closest-integer-sqrt 51298301000395829582.1835 0
+                                            51298301000395829582.1835) 
+                      7162283225.0 "Correct for very large numbers")
+    ))
+
+(run-tests square-root-tests) 
+(run-tests closest-integer-sqrt-tests)
